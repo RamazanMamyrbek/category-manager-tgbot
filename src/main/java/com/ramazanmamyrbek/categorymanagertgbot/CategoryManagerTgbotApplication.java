3221079655d254a4dc8cd5604fd12940ee9b6579
RamazanMamyrbek <rama.mamirbek@gmail.com>
2024-10-18 20:39:34 +0500

    First init

---
+++ src/main/java/com/ramazanmamyrbek/categorymanagertgbot/CategoryManagerTgbotApplication.java
@@ -0,0 +1,13 @@
+package com.ramazanmamyrbek.categorymanagertgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryManagerTgbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryManagerTgbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramazanmamyrbek/categorymanagertgbot/CategoryManagerTgbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramazanmamyrbek.categorymanagertgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryManagerTgbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
